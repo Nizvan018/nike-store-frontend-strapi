166a5e3a9359ee2c817e3b352723bc872ec13471
Nizvan Monteón Ricárdez <njr01397@gmail.com>
2025-04-01 17:39:21 -0600

    Initial commit

---
+++ src/app/components/home/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header className="flex justify-center items-center w-full h-[90vh] bg-blue-zodiac-950 bottom-diagonal">
+            <h1 className="text-white text-9xl font-bold italic">
+                <span className="block">AIR</span>
+                <span className="block -mt-8">JORDAN</span>
+            </h1>
+        </header>
+    )
+}
---
+++ src/app/components/home/Navbar.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { SiNike } from "react-icons/si";
+import { LuUser } from "react-icons/lu";
+import { SlBag } from "react-icons/sl";
+import { usePathname } from "next/navigation";
+
+const links = [
+    { label: "Home", pathname: "/" },
+    { label: "Store", pathname: "/store" },
+    { label: "Contact", pathname: "/contact" }
+]
+
+export default function Navbar() {
+    const pathname = usePathname();
+
+    return (
+        <nav className="z-[1000] fixed flex justify-between items-center gap-12 w-full py-4 px-8 text-white">
+            <div className="w-[120px]">
+                <SiNike size={32} />
+            </div>
+            <ul className="flex items-center gap-8 text-sm">
+                {links.map(item => (
+                    <li key={item.label}>
+                        <a
+                            href={item.pathname}
+                            className={`border-b pb-1 ${item.pathname === pathname ? "border-white" : "border-transparent"}`}
+                        >
+                            {item.label}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+            <div className="flex items-center justify-end gap-4 w-[120px]">
+                <LuUser size={24} />
+                <div className="flex items-center text-blue-zodiac-950 py-1 px-2 rounded-sm bg-white">
+                    <SlBag className="mr-2" />
+                    <span className="border-l border-y-blue-zodiac-950 pl-2">0</span>
+                </div>
+            </div>
+        </nav>
+    )
+}
