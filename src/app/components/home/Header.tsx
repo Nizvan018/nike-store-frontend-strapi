196bc7bc13bd704ecbd74a37db43a7056dc1d662
Nizvan Monteón Ricárdez <njr01397@gmail.com>
2025-04-01 20:46:13 -0600

    Feat(Home Header): Improve the header reponsivity

--- src/app/components/home/Header.tsx
+++ src/app/components/home/Header.tsx
@@ -10,8 +10,8 @@ export default function Header() {
         <header className="flex justify-center items-center w-full h-[60vh] sm:h-[70vh] md:h-[90vh] px-8 bg-gradient-to-br from-blue-zodiac-950 to-slate-950 bottom-diagonal duration-300 overflow-hidden">
             <div className="flex justify-center w-1/2">
                 <h1 className="text-white text-7xl sm:text-8xl md:text-9xl font-bold italic duration-300">
-                    <span className="flex items-center gap-2">AIR <SiNike size={220} /></span>
-                    <span className="block -mt-20">JORDAN</span>
+                    <span className="flex items-center gap-2">AIR <SiNike className="text-9xl sm:text-[160px] md:text-[220px]" /></span>
+                    <span className="block -mt-12 sm:-mt-14 md:-mt-20">JORDAN</span>
                 </h1>
             </div>
             <div className="relative hidden 2xl:flex justify-center items-center w-1/2 h-full">
