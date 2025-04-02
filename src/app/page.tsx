59bddea507588cab7ca3f5e2819f83243a304df5
Nizvan Monteón Ricárdez <njr01397@gmail.com>
2025-04-01 19:28:19 -0600

    Feat(Home page): Add Introduction section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 import Header from "@components/home/Header";
+import Introduction from "./components/home/Introduction";
 
 export default function Home() {
 	return (
-		<Header />
+		<main className="flex flex-col items-center gap-48">
+			<Header />
+
+			<Introduction />
+		</main>
 	);
 }
