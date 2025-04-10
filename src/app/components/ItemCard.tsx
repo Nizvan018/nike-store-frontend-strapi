a0451759551362143e039260ce4d132c831879fb
Nizvan Monteón Ricárdez <njr01397@gmail.com>
2025-04-09 18:04:07 -0600

    Style(ItemCard): Add shine effect to the item card

--- src/app/components/ItemCard.tsx
+++ src/app/components/ItemCard.tsx
@@ -14,7 +14,14 @@ interface Props {
 
 export default function ItemCard({ name, imageSrc, price, coinage, hover, bg, to }: Props) {
     return (
-        <article className={`group w-full aspect-portrait rounded-[14px] ${hover} duration-[2000ms] cursor-pointer`}>
+        <article className={`group relative w-full aspect-portrait rounded-[14px] ${hover} duration-[2000ms] cursor-pointer`}>
+            {/* SHINE EFFECT */}
+            <div className="z-10 absolute top-0 left-0 overflow-hidden w-full h-full rounded-[14px]">
+                <div className="relative w-full h-full">
+                    <div className="absolute w-full h-full -left-[150%] bg-gradient-to-r from-transparent transform-[skewX(-25deg)] via-white/80 to-transparent group-hover:translate-x-[300%] duration-500"></div>
+                </div>
+            </div>
+
             <div className={`w-full h-full p-[2px] rounded-[14px] bg-gradient-to-t from-transparent from-10% ${to}`}>
                 <div className="flex flex-col w-full h-full rounded-xl bg-background">
                     <div className="relative flex justify-center items-center w-full aspect-square">
