5e02e4f6d18ff631c5bb0d72cfc7aeddc42c9dd6
Nizvan MR <njr01397@gmail.com>
2025-04-11 17:22:01 -0600

    Feat(TrendingItems): Add skeleton for the Suspense's fallback

--- src/app/components/home/TrendingItems.tsx
+++ src/app/components/home/TrendingItems.tsx
@@ -1,6 +1,10 @@
 import { PiLightning } from "react-icons/pi";
+import { TbReload } from "react-icons/tb";
 import ItemCard from "../ItemCard";
 import { getFeaturedProducts } from "@/app/api/getFeaturedProducts";
+import Link from "next/link";
+import { Suspense } from "react";
+import TrendingItemsSkeleton from "../skeletons/home/TrendingItemsSkeleton";
 
 const colors = {
     Green: { hover: "hover:bg-lime-500", bg: "bg-lime-300", to: "to-lime-500" },
@@ -13,33 +17,41 @@ export default async function TrendingItems() {
     const res = await getFeaturedProducts();
 
     return (
-        <section className='flex flex-col items-center gap-4 w-full max-w-[1000px]'>
+        <section className='flex flex-col items-center gap-4 w-full max-w-[1000px] px-6'>
             <h2 className='text-2xl font-semibold'>FEATURED ITEMS</h2>
             <PiLightning size={24} />
 
             {/* IF ERROR */}
             {!res.succes && (
-                <span>{res.error}</span>
+                <div className="flex flex-col items-center gap-4 w-full p-8 mt-12 rounded-xl bg-black/5 dark:bg-white/5">
+                    <span className="text-rose-500">{res.error}</span>
+                    <Link href={"/"} className='flex items-center gap-1 text-sm font-medium text-white dark:text-black py-2 px-4 rounded-full bg-black dark:bg-white'>
+                        Reload
+                        <TbReload size={18} />
+                    </Link>
+                </div>
             )}
 
             {/* ITEMS */}
-            <div className="grid grid-cols-3 gap-12 w-full mt-12 pb-8 px-6">
-                {res.succes && res.data.map(item => (
-                    <ItemCard
-                        key={item.documentId}
-                        name={item.name}
-                        price={item.price}
-                        imageSrc={item.images[0].url}
-                        alt={item.images[0].alternativeText}
-                        imageWidth={item.images[0].width}
-                        imageHeight={item.images[0].height}
-                        coinage="USD"
-                        hover={colors[item.color as keyof typeof colors].hover}
-                        bg={colors[item.color as keyof typeof colors].bg}
-                        to={colors[item.color as keyof typeof colors].to}
-                    />
-                ))}
-            </div>
+            <Suspense fallback={<TrendingItemsSkeleton />}>
+                <div className="grid grid-cols-3 gap-12 w-full mt-12 pb-8">
+                    {res.succes && res.data.map(item => (
+                        <ItemCard
+                            key={item.documentId}
+                            name={item.name}
+                            price={item.price}
+                            imageSrc={item.images[0].url}
+                            alt={item.images[0].alternativeText}
+                            imageWidth={item.images[0].width}
+                            imageHeight={item.images[0].height}
+                            coinage="USD"
+                            hover={colors[item.color as keyof typeof colors].hover}
+                            bg={colors[item.color as keyof typeof colors].bg}
+                            to={colors[item.color as keyof typeof colors].to}
+                        />
+                    ))}
+                </div>
+            </Suspense>
         </section>
     )
 }
