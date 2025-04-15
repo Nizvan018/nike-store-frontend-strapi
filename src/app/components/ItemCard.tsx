71ed94ee4e7d9091ad195e1d1ea4ba6493978067
Nizvan Monteón Ricárdez <njr01397@gmail.com>
2025-04-14 18:28:27 -0600

    Style(ItemCard): Add Mono-color colors for the card

--- src/app/components/ItemCard.tsx
+++ src/app/components/ItemCard.tsx
@@ -11,6 +11,7 @@ const colors = {
     Green: { hover: "hover:bg-lime-500", bg: "bg-lime-300", to: "to-lime-500" },
     Blue: { hover: "hover:bg-blue-500", bg: "bg-blue-300", to: "to-blue-500" },
     Red: { hover: "hover:bg-rose-500", bg: "bg-rose-300", to: "to-rose-500" },
+    "Mono-color": { hover: "hover:bg-slate-500", bg: "bg-slate-200", to: "to-slate-500" },
     "Multi-color": { hover: "hover:bg-lime-500", bg: "bg-slate-200", to: "via-rose-500 to-blue-500" }
 }
 
